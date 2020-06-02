7d303344d490d3900fc75ec4c767980b8de5109c
大西雄貴 <yuki.onishi1129@gmail.com>
2020-06-02 14:55:24 +0900

    練習問題2-5

--- chapter-2/src/index.ts
+++ chapter-2/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello TypeScript!');
+console.log('Hello TypeScript!!');
+
+let a: number = 1 + 2;
+let b: number = a + 3;
+let c: {
+  apple: number;
+  banana: number;
+} = {
+  apple: a,
+  banana: b,
+};
+let d: number = c.apple * 4;
